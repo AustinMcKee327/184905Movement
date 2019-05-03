f24a935b4dde44660c3dc25cf6de16b977eacab7
AustinMcKee327 <47399911+AustinMcKee327@users.noreply.github.com>
2019-05-03 11:29:55 -0400

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,8 +24,10 @@ namespace TestingWallsAndPlayers
         public System.Windows.Threading.DispatcherTimer gameTimer;
         double d = 5;
         int i = 0;
-        int speed = 5;
+        double speed = 5;
         Point position = new Point();
+        Point wallposition1 = new Point();
+        Point wallposition2 = new Point();
         Rectangle car = new Rectangle();
         Rectangle Wall = new Rectangle();
 
@@ -39,18 +41,22 @@ namespace TestingWallsAndPlayers
             gameTimer.Tick += GameTimer_Tick;
             gameTimer.Interval = new TimeSpan(0, 0, 0, 0, 1000 / 60);//fps
             gameTimer.Start();
-            Point centre = new Point();
+            wallposition1.Y = 150;
+            wallposition1.X = 0;
+            wallposition2.Y = 200;
             car.Width = 50;
             car.Height = 50;
             car.Fill = Brushes.Aqua;
-            Wall.Width = 50;
-            Wall.Height = 300;
+            Wall.Width = 300;
+            Wall.Height = 50;
             Wall.Fill = Brushes.Black;
+            Track.Children.Add(Wall);
             Track.Children.Add(car);
-            position.X = 0;
-            position.Y = 0;
+            position.X = 100;
+            position.Y = 100;
             Canvas.SetLeft(car, position.X);
             Canvas.SetTop(car, position.Y);
+            Canvas.SetTop(Wall, 200);
             car.RenderTransformOrigin = new Point(0.5, 0.5);
         }
 
@@ -59,25 +65,53 @@ namespace TestingWallsAndPlayers
             if (Keyboard.IsKeyDown(Key.Down))
             {
 
-                position.Y += 10;
+                position.Y += speed * 1;
                 Canvas.SetTop(car, position.Y);
+                if (position.Y >= wallposition1.Y && position.Y <= wallposition2.Y)
+                {
+                    speed = 0.5;
+                    if (position.Y == wallposition2.Y)
+                {
+                    speed = 0;
+                    position.Y -= 10;
+                    Canvas.SetTop(car, position.Y);
+                    
+                }
+                }
                 
-            
+                else
+                {
+                    speed = 5;
+                }
+
             }
             if (Keyboard.IsKeyDown(Key.Up))
             {
-                position.Y -= 10;
+                position.Y -= speed * 1;
                 Canvas.SetTop(car, position.Y);
+                if (position.Y <= wallposition2.Y && position.Y >= wallposition1.Y)
+                {
+                    speed = 0.5;
+                }
+                if (position.Y == wallposition2.Y)
+                {
+                    position.Y += 10;
+                }
+                else
+                {
+                    speed = 5;
+                }
             }
             if (Keyboard.IsKeyDown(Key.Right))
             {
                 car.RenderTransform = new RotateTransform()
                 { Angle = i };
                 i += 10;
-
             }
 
-            }
+
+        }
+            
     }
         
 }
